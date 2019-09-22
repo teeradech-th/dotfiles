08669be0b70a6b89ada599561d89328393436cdc
Teeradech-TH <t.thanapop@gmail.com>
2019-09-22 23:47:53 +0700

    aliase for docker

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,6 @@ alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
 alias tinyhere='tinypng -r . -k DKvYrGr3rpSHLWcnjPSkPQcrFLNKKhsk'
+alias d='docker $*'
+alias dc='docker-compose $*'
+alias de='docker exec $*'
